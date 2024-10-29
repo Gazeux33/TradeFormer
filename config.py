dff59ab73e7b97a9881f83511dcca11a5063e2f0
tcastillo_ubuntu <theocastillo@yahoo.com>
2024-10-29 15:59:01 +0100

    up

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import torch
 
 
 ENDPOINT = "https://paper-api.alpaca.markets/v2"
-PUBLIC = "PKWIHR5UQ4WIYZFTF0UV"
-SECRET = "uMcdPpN2yvAyh7mboLaKonHrfjOKDTIneJDVJFrK"
+PUBLIC = ""
+SECRET = ""
 
 DATA_DIR = "data/"
 CSV_DIR = f"{DATA_DIR}csv_files/"
